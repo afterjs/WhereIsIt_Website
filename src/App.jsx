583ccff29444f71001d9bb4a33027c77bdf48af2
afterjs <ricardoamaroesm@gmail.com>
2021-10-04 22:36:47 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Hello from './Components/Hello'
+
+
+export default (props) => (
+    <div>
+       <Hello/>
+    </div>
+)
---
+++ src/data/products.js
@@ -0,0 +1,5 @@
+export default [
+    {id: 1, name: 'Caneta', preco: 7.59},
+    {id: 2, name: 'Cadenro', preco: 3.23},
+    {id: 3, name: 'Lapis', preco: 2.59},
+]
