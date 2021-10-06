7b52790a218378696ca9926ecddbb7dbf9eeac99
afterjs <ricardoamaroesm@gmail.com>
2021-10-07 00:18:33 +0100

    Created section about team

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from './Components/Navbar'
 import FirstSection from './Components/FirstSection'
+import Download from "./Components/Download";
 import About from "./Components/About";
 
 export default (props) => (
     <div>
         <Navbar></Navbar>
         <FirstSection> </FirstSection>
+        <Download></Download>
         <About></About>
     </div>
 )
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,40 +1,16 @@
 import React from "react";
-import phones from "../Assets/Phones/phoneBlue.png"
-import appStore from "../Assets/Store/appstore.png"
-import playstore from "../Assets/Store/playstore.png"
-
-export default (props) => {
-
-    const phoneStyle = {
-        height: "35rem",
-    }
-
-    const phoneSize = {
-        height: "3rem"
-    }
+import userImage from "../Assets/user.jpg"
+import face from "../Assets/socials/facebook.png"
 
 
+export default props => {
     return (
-      
-            <div className="sectionDonwload">
-                <div className="aligAboutSection">
-                   
-                    <div>
-                        <img src={phones}  style={phoneStyle}/>
-
-                        <div className="storeIconsDiv">
-                        <div>
-                            <img src={appStore} style={phoneSize}/>
-                        </div>
-                        <div>
-                            <img src={playstore} style={phoneSize}/>
-                        </div>
-                    </div>
-                        
-                    </div>
-                    <div className="downloadInformation">
-                        <h1 className="DownloadAppTittle">Lorem Ipsum!</h1>
-                        <p className="DownloadAppJustifyText">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        <div className="sectionAbout">
+            <div className="aligAboutSection">
+                <div className="aboutUsFLex">
+                    <h1 className="about">Sobre Nós</h1>
+                    <div className="aboutText">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                             Quisque eget ante eu ligula iaculis scelerisque in et augue.
                             Maecenas rhoncus convallis massa.
                             Donec urna urna, pretium vitae nisi eget, efficitur tempor diam.
@@ -45,12 +21,66 @@ export default (props) => {
                             Maecenas rhoncus convallis massa.
                             Donec urna urna, pretium vitae nisi eget, efficitur tempor diam.
                             Pellentesque fringilla ipsum a urna dictum, vitae
-                            tincidunt lectus ornare.
-                        </p>
+                            tincidunt lectus ornare.</p>
                     </div>
+
+                    <div className="ImagesDivFlex">
+                        <div>
+
+                            <div className="userProfileFlex">
+                                <div className="img-pad ">
+                                    <img src={userImage} className="userImage" />
+                                </div>
+                                <div>
+                                    <p className="userProfileText">Ricardo Amaro</p>
+                                    <span >Fullstack Developer</span>
+                                </div>
+                                <div>
+                                    <div className="socialsFlex">
+                                        <div className="socialSpace">
+                                            <img src={face} className="face" />
+                                        </div>
+                                        <div className="socialSpace">
+                                            <img src={face} className="face" />
+                                        </div>
+                                        <div className="socialSpace">
+                                            <img src={face} className="face" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div>
+                            <div className="userProfileFlex">
+                                <div className="img-pad ">
+                                    <img src={userImage} className="userImage" />
+                                </div>
+                                <div>
+                                    <p className="userProfileText">Ricardo Amaro</p>
+                                    <span >Fullstack Developer</span>
+                                </div>
+                                <div>
+                                    <div className="socialsFlex">
+                                        <div className="socialSpace">
+                                            <img src={face} className="face" />
+                                        </div>
+                                        <div className="socialSpace">
+                                            <img src={face} className="face" />
+                                        </div>
+                                        <div className="socialSpace">
+                                            <img src={face} className="face" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
-
+        </div>
     )
 
 }
