d201ef8eba79e15892ba353230f90188f123734e
afterjs <ricardoamaroesm@gmail.com>
2021-10-06 16:11:40 +0100

    Fix some problems with pin locations

--- src/Components/Layout/MarkerMap.jsx
+++ src/Components/Layout/MarkerMap.jsx
@@ -4,15 +4,36 @@ import '../Layout/MarkerMap.css'
 
 
 export default props => {
+
+    let pop = () => {
+
+        return <Popup>
+            <div className="al-ce">
+                <p className={props.classe}>{props.name}</p>
+                <div>
+                    <div>
+                        <span className="specsCoord">Latitude:</span> {props.lat}
+                    </div>
+                    <div>
+                        <span className="specsCoord" > Longitude: </span>{props.long}
+                    </div>
+                </div>
+
+                <div className="pd-div">
+                    <button type="button" className="btn btn-dark">
+                        Direção
+                    </button>
+                </div>
+            </div>
+
+            {/**/}
+        </Popup>
+
+    }
+
     return (
         <>
-            <Marker position={[props.lat, props.long]} icon={props.icon}>
-                <Popup> <span className={props.classe}> {props.name}</span><br/>
-                    Latitude: {props.lat}
-                    <br/>
-                    Lontitude: {props.long}
-                </Popup>
-            </Marker>
+            <Marker position={[props.lat, props.long]} icon={props.icon}>{pop()}</Marker>
         </>
     )
 }
--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -12,42 +12,38 @@ export default props => {
     let DEFAULT_LONGITUDE = -8.834282105916813;
 
 
-  
-    const lixoIcon = new L.Icon({
-        iconUrl: lixo,
-        iconRetinaUrl: lixo,
-        popupAnchor:  [-0, -0],
-        iconSize: [36,50],     
-    });
+    const icons = {
+        lixoIcon : new L.Icon({
+            iconUrl: lixo,
+            iconRetinaUrl: lixo,
+            popupAnchor:  [-0, -0],
+            iconSize: [36,50],     
+        }),
+        caixaIcon : new L.Icon({
+            iconUrl: caixa,
+            iconRetinaUrl: caixa,
+            popupAnchor:  [-0, -0],
+            iconSize: [36,50],     
+        })
+    }
 
-    const caixaIcon = new L.Icon({
-        iconUrl: caixa,
-        iconRetinaUrl: caixa,
-        popupAnchor:  [-0, -0],
-        iconSize: [36,50],     
-    });
+  
+
 
 
     function getMapData() {
         return data.map(schema => {
             let newIcon = null
-            schema.icon===1 ? newIcon = lixoIcon : newIcon = caixaIcon
-            return  <MarkerMap key={schema.id} lat={schema.lat} long={schema.long} icon={newIcon} classe={schema.classe} name={schema}></MarkerMap>
+            schema.icon===1 ? newIcon = icons.lixoIcon : newIcon = icons.caixaIcon
+            return  <MarkerMap key={schema.id} lat={schema.lat} long={schema.long} icon={newIcon} classe={schema.classe} name={schema.name}></MarkerMap>
         })
     }
-
-
+   
     return (
         <>
             <MapContainer center={[DEFAULT_LATITUDE, DEFAULT_LONGITUDE]} zoom={14} scrollWheelZoom={true} attributionControl={false} >
-                <TileLayer
-                    url="https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=ReDR1oFtIdDrGNpoYpt3"
-                />
-
-
-               {getMapData()}
-               
-                
+                <TileLayer url="https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=ReDR1oFtIdDrGNpoYpt3"/>
+                {getMapData()}
             </MapContainer>
         </>
     )
