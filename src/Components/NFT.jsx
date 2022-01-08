71030579370bffd7182aae0bb45ab24bb4a73f16
afterjs <ricardoamaroesm@gmail.com>
2022-01-08 01:06:29 +0000

    updaded nfts

--- src/Components/NFT.jsx
+++ src/Components/NFT.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import nft3 from "../Assets/NFTs/nft3.png"
-import nft1 from "../Assets/NFTs/nft1.png"
-import nft2 from "../Assets/NFTs/nft2.png"
-import nft4 from "../Assets/NFTs/nft4.png"
+import nft3 from "../Assets/NFTs/nft3.jpg"
+import nft1 from "../Assets/NFTs/nft1.jpg"
+import nft2 from "../Assets/NFTs/nft2.jpg"
+import nft4 from "../Assets/NFTs/nft4.jpg"
 
 export default props => {
     return (
@@ -11,15 +11,17 @@ export default props => {
             <div className="nftFlex">
                 <div className="nftImages">
                     <img src={nft3} className="userImage nftImage"></img>
-                    <p className="nftText">NFTs</p>
-                    <img src={nft1} className="userImage nftImage"></img>
-                    <p className="nftText">NFTs</p>
+                    <p className="nftText">#1 Serra da Estrela</p>
+                    <img src={nft2} className="userImage nftImage"></img>
+                    <p className="nftText">#2 Praia da Areia</p>
                 </div>
                 <div className="nftImages">
-                    <img src={nft2} className="userImage nftImage"></img>
-                    <p className="nftText">NFTs</p>
+                   
+
+                    <img src={nft1} className="userImage nftImage"></img>
+                    <p className="nftText">#3 Anos 2000</p>
                     <img src={nft4} className="userImage nftImage"></img>
-                    <p className="nftText">NFTs</p>
+                    <p className="nftText">#4 водка</p>
                 </div>
             </div>
         </div>
